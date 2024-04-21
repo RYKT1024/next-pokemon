6a9b74fb490098db8137d9a3a0f9379f2330d779
RYKT <rykt1024@163.com>
2024-04-21 19:24:08 +0800

    optimize pokemonui

--- src/ui/pokemon/pokemon.tsx
+++ src/ui/pokemon/pokemon.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { fetchPokemonName, fetchPokemonImage } from '@/lib/data';
+import { fetchPokemonName } from '@/lib/data';
 import PokemonUI from './pokemonUI';
 import PokemonButton from './changePokemon';
 import SelectPokemon from './selectPokemon';
@@ -11,25 +11,25 @@ export default function Pokemon({id} : {
   id:string;
 }) {
 
-  const [img, setImg] = useState('');
   const [name, setName] = useState('');
   const [refresh, setRefresh] = useState(false);
   const [language, setLanguage] = useLocalStorage<string>('language', 'zh-Hans');
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    Promise.all([
-      fetchPokemonImage(id, 'front_default'),
-      fetchPokemonName(id, language)
-    ]).then(([img, name]) => {
-      setImg(img);
+    fetchPokemonName(id, language).then((name) => {
       setName(name);
-    })
+      setLoaded(true);
+    });
+    return () => {
+      setLoaded(false);
+    }
   }, [id, refresh, language])
   
     return (
       <>
         <div className='pt-12 flex flex-col items-center'>
-          <PokemonUI id={id} img={img} name={name} refresh={refresh}/>
+          <PokemonUI id={id} name={name} refresh={refresh} pkLoaded={loaded}/>
           <div className='flex pt-4'>
             <PokemonButton id={(Number(id)-1).toString()} sKey='KeyZ'>
               &lt;</PokemonButton>
--- src/ui/pokemon/pokemonUI.tsx
+++ src/ui/pokemon/pokemonUI.tsx
@@ -1,24 +1,25 @@
 'use client'
 
 import BlackWhiteImg from "@/ui/common/blackWhiteImg"
-import { fetchPokemonFounded } from "@/lib/data"
+import { fetchPokemonFounded, fetchPokemonImage } from "@/lib/data"
 import { useState, useEffect } from "react"
 import useLocalStorage from "@/lib/local"
 
 import Image from 'next/image'
 
-export default function PokemonUI({id, img, name, refresh, className}: {
-  id:string, img: string, name: string, refresh: boolean ,className?: string
+export default function PokemonUI({id, name, refresh, pkLoaded, className}: {
+  id:string, name: string, refresh: boolean, pkLoaded: boolean, className?: string
 }) {
+  const [img, setImg] = useState('') 
   const [showPokemon, setShowPokemon] = useState(false)
   const [loaded, setLoaded] = useState(false)
   const [localShowPokemonKey,] = useLocalStorage<string>("showPokemonKey", "KeyP")
   const [localShowPokemon,] = useLocalStorage<boolean>("showPokemon", true)
 
   useEffect(() => {
-    if (img)
-    fetchPokemonFounded(1, id).then(res => {
-      setShowPokemon(res>0 && localShowPokemon);
+    Promise.all([fetchPokemonFounded(1, id), fetchPokemonImage(id, 'front_default')]).then(([foundedRes, imgRes]) => {
+      setShowPokemon(foundedRes>0 && localShowPokemon);
+      setImg(imgRes);
       setLoaded(true);
     })
     // 处理键盘按键事件
@@ -33,6 +34,7 @@ export default function PokemonUI({id, img, name, refresh, className}: {
     // 组件卸载时移除事件监听器
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
+      setLoaded(false);
     };
   }, [id, img, refresh, localShowPokemonKey, localShowPokemon]);
   
@@ -40,23 +42,23 @@ export default function PokemonUI({id, img, name, refresh, className}: {
     <div className={`${className} w-80 h-full rounded-3xl shadow-2xl bg-white flex flex-col items-center`}>
       <div className='h-80 mt-6 mb-4 flex flex-col items-center text-center'>
         {
-        loaded ?
-          !showPokemon ? (
-          <>
-            <p className='h-16 mt-4 text-3xl font-bold text-gray-800'>*未发现*</p> 
-            <BlackWhiteImg className='w-64 h-64 select-none'
-                          width={256} height={256}
-                          src={img} alt='未发现的宝可梦' />
-          </>
-          ) : (
-          <>
-            <p className='h-16 mt-4 text-3xl font-bold text-gray-800'>{name}</p> 
-            <Image  className='w-64 h-64 select-none'
-                    width={256} height={256}
-                    src={img} alt={name} 
-                    draggable='false' />
-          </>
-          )
+          loaded ?
+            !showPokemon ? (
+              <>
+                <p className='h-16 mt-4 text-3xl font-bold text-gray-800'>*未发现*</p> 
+                <BlackWhiteImg className='w-64 h-64 select-none'
+                              width={256} height={256}
+                              src={img} alt='未发现的宝可梦' />
+              </>
+              ) : (
+              <>
+                <p className='h-16 mt-4 text-3xl font-bold text-gray-800'>{pkLoaded ? name : ''}</p> 
+                <Image  className='w-64 h-64 select-none'
+                        width={256} height={256}
+                        src={img} alt={name} 
+                        draggable='false' />
+              </>
+            )
           : (
             <></>
           )
